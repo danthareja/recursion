f41d942ff3fcb45887c645cc0ddc76cfc3ba136f
Hack Reactor Students <danthareja@gmail.com>
2014-07-21 15:28:48 -0700

    basic structure

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,6 +4,28 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className){
+var getElementsByClassName = function(className,node){
   // your code here
+
+  var results = [];
+
+  //identify starting node
+  node = node || document.body;
+
+  //check if node contains className
+  if (node.classList.contains(className)){
+    results.push(node);
+  }
+
+  //check if node has children
+  if (node.children.length) {
+    //iterate over children
+    for (var i =0,length=node.children.length; i<length; i++) {
+    //invoke recursive function
+    getElementsByClassName(className,node.children[i]);
+   }
+  }
+
+  //return array of results
+  return results;
 };
